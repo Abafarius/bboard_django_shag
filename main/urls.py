3f0c1439d73398be495c1de76004ca039f9f10e5
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-12-24 20:20:48 +0500

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import index,  other_page,  BBLoginView, profile, BBLogoutView, ProfileEditView, BBPasswordChangeView
+from .views import index,  other_page,  BBLoginView, profile, BBLogoutView, ProfileEditView, PasswordEditView, \
+    RegisterView, RegisterDoneView, user_activate, ProfileDeleteView
 
 
 app_name = 'main'
@@ -14,5 +15,10 @@ urlpatterns = [
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/profile/edit/', ProfileEditView.as_view(), name='profile_ edit'),
     path('accounts/password/edit/', PasswordEditView.as_view(), name='password_ edit'),
+    path('acoounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('acoounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/асtivate/<str:sign>/', user_activate, name='activate'),
+    path('accounts/profile/delete/', ProfileDeleteView.as_view(), name='profile_delete'),
+
 ]
 
